1a9fbe9d25eaaa0ecca90d0251aa1407a01ad6d1
金田 <kaneda@dev.null>
2016-08-16 15:14:47 +0200

    Limit cargo.cs debug logging to debug builds only Started work on FleetAutodiscover, meant as replacement for GroupControl

---
+++ EveComFramework/GroupControl/FleetAutodiscover.cs
@@ -0,0 +1,43 @@
+﻿using EveCom;
+using LavishScriptAPI;
+
+namespace EveComFramework.GroupControl
+{
+    /// <summary>
+    /// Fleet autodiscovery
+    /// </summary>
+    public class FleetAutodiscover
+    {
+        /// <summary>
+        /// Request fleet invite from booster
+        /// </summary>
+        public static void Request()
+        {
+            if (Session.InFleet) return;
+
+            try
+            {
+                LavishScript.ExecuteCommand("relay \"all other\" FleetAutodiscoverRequest " + Me.CharID + " " + Session.SolarSystemID);
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Respond to fleet invite request, telling the character to accept a fleet invite once
+        /// </summary>
+        /// <param name="CharacterID"></param>
+        public static void Respond(long CharacterID)
+        {
+            if (Session.InFleet) return;
+
+            try
+            {
+                LavishScript.ExecuteCommand("relay \"all other\" FleetAutodiscoverResponse " + Me.CharID + " " + CharacterID);
+            }
+            catch { }
+
+            // @TODO: Send Fleet Invite
+        }
+
+    }
+}
